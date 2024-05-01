762a131ec9ba8132c836a3d8726aa321a54a62be
miglesiasDev <miglesias.dev@gmail.com>
2024-05-01 19:16:40 -0400

    Prueba de campo mi cuenta

--- src/pages/MiCuenta/index.jsx
+++ src/pages/MiCuenta/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectIsLogged, selectUserLogged } from "../../features/user/userSlice";
 
 
 const MiCuentaPage = () => {
+
+  const user = useSelector(selectUserLogged)
+  console.log(user)
   return (
     <div>
       <Layout />
