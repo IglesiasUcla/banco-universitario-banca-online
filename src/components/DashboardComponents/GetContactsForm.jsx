b4b6c67c81e884d96cb7aedcd4be04f70f754cbf
miglesiasDev <miglesias.dev@gmail.com>
2024-05-07 02:28:56 -0400

    Modificaciones estéticas

--- src/components/DashboardComponents/GetContactsForm.jsx
+++ src/components/DashboardComponents/GetContactsForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ContactAPI from "../../api/ContactAPI/index";
 import Row from "../ContactComponents/TableContactComponents/Row";
 import { Link as LinkRouter } from "react-router-dom";
@@ -6,18 +6,23 @@ import { Link as LinkRouter } from "react-router-dom";
 const GetContactsForm = () => {
   const contactAPI = new ContactAPI();
   const [contacts, setContacts] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
     const fetchContacts = async () => {
       try {
-        const { data } = await contactAPI.getAll();
+        const { data } = await contactAPI.getAll(search);
         setContacts(data.slice(0, 2)); // Obtener los primeros dos contactos
       } catch (error) {
         console.error(error);
       }
     };
     fetchContacts();
-  }, []);
+  }, [search]);
 
   return (
     <div className="box-content rounded bg-white w-[95%] h-full my-5 flex flex-col p-5 justify-start space-y-3">
@@ -29,14 +34,19 @@ const GetContactsForm = () => {
       </div>
       <div className="overflow-auto w-auto h-full space-y-3">
         {contacts.length > 0 ? (
-          contacts.map((contact, index) => (
-            <Row key={index} index={index} contact={contact} showActions={index >= 2} />
-          ))
+          <div className="rounded border-2 bg-white border-[#eff0f4] min-w-96 w-full h-auto flex justify-between items-center p-2">
+            <span className="w-[20%] font-bold">Alias</span>
+            <span className="w-[20%] font-bold">Número de cuenta</span>
+            <span className="w-[20%] font-bold">Descripción</span>
+          </div>
         ) : (
           <div className="rounded border-2 bg-white border-[#eff0f4] min-w-96 w-full h-auto flex justify-between items-center p-2">
             <span className="w-full">No hay contactos disponibles...</span>
           </div>
         )}
+        {contacts.map((contact, index) => (
+          <Row key={index} index={index} contact={contact} showActions={index >= 2} />
+        ))}
       </div>
     </div>
   );
